7324d25dbcad9064c92559066b39c8a79a9c78ea
lordPachy <francescocalzona03@gmail.com>
2024-03-18 15:55:05 +0100

    DMAA 18-03

--- Data Management/Class practice/Scripts/mtcars_exercises.R
+++ Data Management/Class practice/Scripts/mtcars_exercises.R
@@ -13,7 +13,7 @@ ggplot(data = mtcars, mapping = aes(x = wt, y = mpg)) + geom_point() + geom_smoo
 ggplot(data = mtcars, mapping = aes(x = hp, y = qsec)) + geom_point(mapping = aes(color = vs)) + geom_smooth(method = "lm")
 
 #Exercise 3
-ggplot(mtcars, aes(y = mpg, x = wt), color = cyl) + geom_point() + facet_grid(~gear) + geom_smooth(method = "lm") + theme_dark()
+ggplot(mtcars, aes(y = mpg, x = wt, color = cyl)) + geom_point() + facet_grid(~gear) + geom_smooth(method = "lm") + theme_minimal()
 
 #Exercise 4
 mode(mtcars)
@@ -22,7 +22,7 @@ summary(fourcyl)
 head(fourcyl)
 sixcyl = dplyr::filter(mtcars, cyl == 6)
 summary(mtcars)
-ggplot(mtcars, aes(x = mpg)) + geom_boxplot() + facet_wrap(~cyl)
+ggplot(mtcars, aes(y = mpg)) + geom_boxplot() + facet_wrap(~cyl)
 ggplot(mtcars, aes(x = cyl, y = mpg)) + geom_violin() + facet_wrap(~cyl)
 mtcars[mtcars$cyl == 4, ]
 t.test(mtcars[mtcars$cyl == 4, ]$mpg, mtcars[mtcars$cyl == 6, ]$mpg)
