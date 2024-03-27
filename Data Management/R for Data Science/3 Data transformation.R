197cde719368a2b4e65447de9e92443568143baa
lordPachy <francescocalzona03@gmail.com>
2024-03-27 17:05:32 +0100

    Sync desktop

--- Data Management/R for Data Science/3 Data transformation.R
+++ Data Management/R for Data Science/3 Data transformation.R
@@ -1,5 +1,12 @@
-#SECTIONS 3.1-
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
+
+# SECTION 3.1
+
+```{r}
 library(nycflights13)
-library(tidyverse)
-#View is useful to scroll datasets
-View(flights)
+library(tidyverse) #View is useful to scroll datasets View(flights)
+```
