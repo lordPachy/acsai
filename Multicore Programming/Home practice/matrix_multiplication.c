81f2f4146b83a5d570c9d46fc8e525a603c71ecd
lordPachy <francescocalzona03@gmail.com>
2024-10-29 10:51:22 +0100

    Continued matrix multiplication

--- Multicore Programming/Home practice/matrix_multiplication.c
+++ Multicore Programming/Home practice/matrix_multiplication.c
@@ -44,7 +44,8 @@ void print_matrix(int** m, int num_rows, int num_cols){
             printf("%d ", m[i][j]);
     }
 
-    printf("\n");    
+    printf("\n");
+    printf("I am not blocked anymore");
 }
 
 int** matrix_matrix_mul(int** m1, int num_rows_1, int num_cols_1, int** m2, int num_rows_2, int num_cols_2){
@@ -76,48 +77,72 @@ int main(int argc, char** argv){
 
         // MPI initialization
         int r = MPI_Init(&argc, &argv);
-        int size, rank;                 // MPI variables
+        int size, rank;     
 
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-        if (rank == size - 1){
+        
+        if (r != MPI_SUCCESS){
+            printf("Error starting MPI program. Terminating.\n");
+            MPI_Abort(MPI_COMM_WORLD, r);
+        } else {
             // First, we store the matrix dimensions
             int num_rows_1 = atoi(argv[1]);
             int num_cols_1 = atoi(argv[2]);
             int num_rows_2 = atoi(argv[3]);
             int num_cols_2 = atoi(argv[4]);
 
-            // We can now create the 2 matrices and print them
-            int** m1 = random_matrix(num_rows_1, num_cols_1);
-            print_matrix(m1, num_rows_1, num_cols_1);
+            // let's assume the number of rows is divisible by the number of threads, for now
+            int** m1 = allocate_matrix(num_rows_1, num_cols_1);
+            int** m2 = allocate_matrix(num_rows_2, num_cols_2);
 
-            int** m2 = random_matrix(num_rows_2, num_cols_2);
-            print_matrix(m2, num_rows_2, num_cols_2);
+            if (rank == size - 1){
+                // We can now create the 2 matrices and print them
+                m1 = random_matrix(num_rows_1, num_cols_1);
+                printf("I'm rank %d, and the original matrices are:\n", rank);
+                print_matrix(m1, num_rows_1, num_cols_1);
 
-            // Sequential execution
-            int** m = matrix_matrix_mul(m1, num_rows_1, num_cols_1, m2, num_rows_2, num_cols_2);
-            print_matrix(m, num_rows_1, num_cols_2);
+                m2 = random_matrix(num_rows_2, num_cols_2);
+                print_matrix(m2, num_rows_2, num_cols_2);
+
+                // Sequential execution
+                int** m = matrix_matrix_mul(m1, num_rows_1, num_cols_1, m2, num_rows_2, num_cols_2);
+                print_matrix(m, num_rows_1, num_cols_2);
+                }
+
+            // Communication: sending the i-th num_rows_1/size rows to the first num_rows_1/size rows of thread i, on m1
+            for(int thread = 0; thread < size; thread++){   
+                for (int i = 0; i < num_rows_1/size; i++){ 
+                     
+                    MPI_Sendrecv(m1[thread * num_rows_1/size + i], num_cols_1, MPI_INT, thread, 0,
+                                 m1[i], num_cols_1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);  // Matrices must be sent row by row
+                }
             }
 
-            MPI_Scatter( const void* sendbuf , num_rows_1/p, MPI_int , void* recvbuf , num_rows_1/p, MPI_Datatype recvtype , int root , MPI_Comm comm);
+            // Communication : just broadcast matrix 2
+            for(int i = 0; i < num_rows_2; i++){      
+                MPI_Bcast(m2[i], num_cols_2, MPI_INT, size - 1, MPI_COMM_WORLD);
+            }
 
-        
+            // Matrix multiplication
+            int** m = matrix_matrix_mul(m1, num_rows_1/size, num_cols_1,
+                                        m2, num_rows_2, num_cols_2);
 
-        // Parallel execution
-        int r = MPI_Init(NULL, NULL);
-        int size, rank;                 // MPI variables
+            print_matrix(m, num_rows_1/size, num_cols_2);
+            
+            // Communication: sending the first num_rows_1/size rows from thread i to the i-th num_rows_1/size rows of thread 0, on m
+            for(int thread = 1; thread < size; thread++){   
+                for (int i = 0; i < num_rows_1/size; i++){   
+                    MPI_Sendrecv(m[i], num_cols_2, MPI_INT, 0, 0,
+                                 m[thread * num_rows_1/size + i], num_cols_2, MPI_INT, thread, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                }
+            }
 
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+            // Final print from rank 0
+            if (rank == 0){
+                print_matrix(m, num_rows_1, num_cols_2);
+            }
 
-        int my_num_samples = num_samples/size;      // number of samples per thread
-
-        if (r != MPI_SUCCESS){
-            printf("Error starting MPI program. Terminating.\n");
-            MPI_Abort(MPI_COMM_WORLD, r);
-        }
-        else{
         }
 
         MPI_Finalize();
