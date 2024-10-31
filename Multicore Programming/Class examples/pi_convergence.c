d1e02eda6fd200667a251cfe15f721abb8285d5c
lordPachy <francescocalzona03@gmail.com>
2024-10-31 10:13:51 +0100

    Finished pi_convergence and started iterative stencil

--- Multicore Programming/Class examples/pi_convergence.c
+++ Multicore Programming/Class examples/pi_convergence.c
@@ -6,21 +6,40 @@
 
 int NUM_CORES = 2;
 
+// compute_partial_pi_series() needs its args in a struct. result is the return value and it is modified in-place
+
 typedef struct compute_pps_args{
     int num_cores;
     int index;
-    int terms_number;
+    int num_terms;
     float result;
 }pps_args;
 
+/**
+ * A function that computes a portion of the pi series iteratively.
+ * 
+ * Input:
+ * - void* args: since this function will be passed to pthreads, it requires a void*-casted of a pointer to a compute_pps_args object.
+ * 
+ * Output:
+ * - void* to NULL, but the result field of compute_pps_args will be modified with the result.
+ */
 void* compute_partial_pi_series(void* args){
+    // We re-cast args to its proper type and create internal variables for readibility
     pps_args* params = (pps_args*) args;
-    params->result = 0;
-    for (int i = 0; i < (params->terms_number)/(params->num_cores); i++){
-        params->result += pow((double) -1, (double) i) / (params->num_cores * i + params->index + 1);
-    }
+    float partial_result = params->result;
+    int num_cores = params->num_cores;
+    int num_terms = params->num_terms;
+    int offset = params->index;
+    int n;      // this is the new index, multiplied by the lowered frequency (thanks to the higher number of cores) and correctly offset
 
-    printf("The partial result inside the functions is %f\n", params->result);
+    // Pi iterative partial calculation
+    for (int i = 0; i < num_terms/num_cores; i++){
+        n = num_cores * i + offset;
+        partial_result += pow((double) -1, (double) n) / (2 * n + 1);
+    }
+    params->result = 4 * partial_result;
+
     return NULL;
 }
 
@@ -31,28 +50,33 @@ int main(int argc, char** argv){
         // We first need to decide how many terms of the series we need
         int terms_number = atoi(argv[1]);
 
-        // Variable initialization
+        // Variables initialization
         float total = 0;
-        void* params_void;
         pthread_t** thread_handles = malloc(sizeof(pthread_t*) * NUM_CORES);
         pps_args* params = malloc(sizeof(pps_args) * NUM_CORES);
 
         for (int i = 0; i < NUM_CORES; i++){
-            // Initializing the parameters for the function
-            params[i] = (pps_args) {NUM_CORES, i, terms_number, 0};
-            params_void = (void*) &params[i];
-
             // Initializing the thread handle
             thread_handles[i] = malloc(sizeof(pthread_t));
 
+            // Initializing the parameters for the function
+            params[i] = (pps_args) {NUM_CORES, i, terms_number, 0};
+
             // Creating the thread
-            pthread_create(thread_handles[i], NULL, compute_partial_pi_series, params_void);
+            pthread_create(thread_handles[i], NULL, compute_partial_pi_series, (void*) &params[i]);
         }
 
         // Summing up sequentially all the results
         for (int i = 0; i < NUM_CORES; i++){
+            // We need to wait for the thread to finish
+            pthread_join(*thread_handles[i], NULL);
             total += params[i].result;
+            free(thread_handles[i]);
         }
+
+        // Freeing up allocated now-useless pointers
+        free(thread_handles);
+        free(params);
 
         printf("The estimate of pi is %f\n", total);
     }
--- Multicore Programming/Home practice/2d_iterative_stencil.c
+++ Multicore Programming/Home practice/2d_iterative_stencil.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-int** allocate_matrix(int num_rows, int num_cols){
-    // It returns a dinamically allocated memory space of dimension
-    // num_rows x num_cols. It returns a pointer to a vector of pointers,
-    // pointing to each future row.
-
+/**
+ * It allocates the matrix dinamically, as a pointer to a vector of pointers.
+ * 
+ * Input:
+ * - int num_rows: a non-zero positive integer
+ * - int num_cols: a non-zero positive integer
+ * 
+ * Output:
+ * - int**
+ */
+int** allocate_int_matrix(int num_rows, int num_cols){
     int** m;       
 
     m = (int**) malloc(sizeof(int*)*num_rows);
@@ -18,12 +24,19 @@ int** allocate_matrix(int num_rows, int num_cols){
     return m;
 }
 
-int** random_matrix(int num_rows, int num_cols){
-    // It returns a dinamically allocated random matrix of dimension
-    // num_rows x num_cols. It returns a pointer to a vector of pointers,
-    // pointing to each row.
-
-    int** m = allocate_matrix(num_rows, num_cols);
+/**
+ * It returns a dinamically allocated matrix full of random ints between
+ * 0 and 100. It requires stdlib.h and time.h.
+ * 
+ * Input:
+ * - int num_rows: a non-zero positive integer
+ * - int num_cols: a non-zero positive integer
+ * 
+ * Output:
+ * - int**
+ */
+int** random_int_matrix(int num_rows, int num_cols){
+    int** m = allocate_int_matrix(num_rows, num_cols);
     unsigned int seed = time(0);    // taking current time as seed        
 
     for (int i = 0; i < num_rows; i++){
@@ -35,9 +48,18 @@ int** random_matrix(int num_rows, int num_cols){
     return m;
 }
 
-void print_matrix(int** m, int num_rows, int num_cols){
-    // It supports matrices in double pointer form.
-
+/**
+ * It prints dinamically allocated matrix stored in a pointer to pointers.
+ * 
+ * Input:
+ * - int** m: the matrix to be printed
+ * - int num_rows: the function will print rows in the range [0, num_rows). It must be less or equal to the real number of rows of m
+ * - int num_cols: the function will print columns in the range [0, num_cols). It must be less or equal to the real number of columns of m
+ * 
+ * Output:
+ * - NULL
+ */
+void print_int_matrix(int** m, int num_rows, int num_cols){
     for (int i = 0; i < num_rows; i++){
         printf("\n");
         for (int j = 0; j < num_cols; j++)
@@ -48,13 +70,66 @@ void print_matrix(int** m, int num_rows, int num_cols){
     fflush(stdout);         // Sometimes, prints are stored in a buffer that needs to flushed out
 }
 
+/**
+ * This function applies the operation of cardinal sum on a single element (i, j), defined as,
+ * A'[i][j] = A[i+1][j] + A[i][j+1] + A[i-1][j] + A[i][j-1] (with A[h][k] = 0 if undefined)
+ * by using A = m1 and A' = m2. Thus, it is not performed in place. m2 is unchanged apart for m2[i][j].
+ * 
+ * Input:
+ * - int** m1: the original matrix
+ * - int** m2: the matrix on which the cardinal sum is applied. Note how m1 and m2 may not be equal before the function is applied.
+ * - int num_rows: the number of rows of both matrices
+ * - int num_rows: the number of columns of both matrices
+ * - int i: row of the element where the cardinal sum will be applied
+ * - int j: column of the element where the cardinal sum will be applied
+ */
+void element_cardinal_sum(int** m1, int** m2, int num_rows, int num_cols, int i, int j){
+    m2[i][j] = 0;
+
+    for (int k = 0; k < 2; k++){
+        if (i - 1 + 2*k >= 0 && i - 1 + 2*k < num_rows){
+            for (int h = 0; h < 2; h++){
+                if (j - 1 + 2*h >= 0 && j - 1 + 2*h < num_cols){
+                    m2[i][j] += m1[i - 1 + 2*k][j - 1 + 2*h];
+                }
+            }
+        }
+    }
+}
+
+/**
+ * This function creates a dinamically allocated copy of a dinamically allocated matrix of ints.
+ * 
+ * Input:
+ * - int** m1: the matrix to be copied
+ * - int num_rows: the number of rows of m1
+ * - int num_cols: the number of colums of m1
+ * 
+ * Output:
+ * - int**: a pointer to the copied matrix
+ */
+int** copy_int_matrix(int** m1, int num_rows, int num_cols){
+    int** m2 = allocate_int_matrix(num_rows, num_cols);
+
+    for (int i = 0; i < num_rows; i++){
+        for (int j = 0; j < num_cols; j++){
+            m2[i][j] = m1[i][j];
+        }
+    }
+
+    return m2;
+}
+
 int main(int argc, char** argv){
     if (argc != 4){
         printf("Wrong number of matrix dimensions\n");
     } else {
         // MPI initialization
         int r = MPI_Init(&argc, &argv);
-        int size, rank, num_rows, num_cols, num_iter;     
+        int size, rank, num_rows, num_cols, num_iter;
+        int** m;
+        int** m1;
+        int** m2;     
 
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -67,6 +142,14 @@ int main(int argc, char** argv){
             num_rows = atoi(argv[1]);
             num_cols = atoi(argv[2]);
             num_iter = atoi(argv[3]);
+
+            // We can now instantiate the matrix
+            if (rank == 0){
+                m = random_int_matrix(num_rows, num_cols);
+                m1 = copy_int_matrix(m, num_rows, num_cols);
+                print_int_matrix(m, num_rows, num_cols);
+                print_int_matrix(m1, num_rows, num_cols);
+            }
         }
         MPI_Finalize();
     }
